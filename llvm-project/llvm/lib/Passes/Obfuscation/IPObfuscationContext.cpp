bf8595d8ae909792854b954848ce16b6cd91b639
0x1306a94 <0x1306a94@gmail.com>
2025-09-25 08:38:05 +0800

    添加ohos NDK 支持

--- llvm-project/llvm/lib/Passes/Obfuscation/IPObfuscationContext.cpp
+++ llvm-project/llvm/lib/Passes/Obfuscation/IPObfuscationContext.cpp
@@ -229,8 +229,11 @@ namespace llvm
             Call->eraseFromParent();
         }
 
-        // NF->getBasicBlockList().splice(NF->begin(), F->getBasicBlockList());
+#if LLVM_VERSION_MAJOR >= 17
         NF->splice(NF->begin(), F);
+#else
+        NF->getBasicBlockList().splice(NF->begin(), F->getBasicBlockList());
+#endif
         // Loop over the argument list, transferring uses of the old arguments over to
         // the new arguments, also transferring over the names as well.
         Function::arg_iterator I2 = NF->arg_begin();
